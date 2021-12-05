5444adcf3ed66094cebf8a49af143c6dec730c3b
snehk <sneh.kothari28@gmail.com>
2021-12-05 18:13:40 +0530

    1

---
+++ src/test/java/Helloworldtest.java
@@ -0,0 +1,9 @@
+public class Helloworldtest {
+    public void testme() {
+        System.out.println("Running test!!!!!!!!");
+    }
+    public void testme2() {
+        System.out.println("Running test2!!!!!!!!");
+    }
+
+}
